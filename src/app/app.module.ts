98465dbde1c98f0b312b59c1e150f4e434fb085c
anil suryavanshi <thakurinfotech@gmail.com>
2017-03-13 12:42:59 +0530

    Sync with response from server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule, Routes } from '@angular/router';
-
+import { JsonpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { MyWelcomeComponentComponent } from './my-welcome-component/my-welcome-component.component';
 import { MyGisComponentComponent } from './my-gis-component/my-gis-component.component';
-
+import { MyCourseServiceService } from './my-course-service.service';
 const appRoutes: Routes = [
   { path: 'main', component: MyWelcomeComponentComponent },
   { path: 'myjs',      component: MyGisComponentComponent },
@@ -30,10 +30,11 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
+    JsonpModule,
     NgbModule.forRoot(),
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [MyCourseServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/my-gis-component/my-gis-component.component.ts
+++ src/app/my-gis-component/my-gis-component.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MyCourseServiceService } from '../my-course-service.service';
+import {Courses} from '../data';
+
+
 @Component({
   selector: 'app-my-gis-component',
   templateUrl: './my-gis-component.component.html',
-  styleUrls: ['./my-gis-component.component.css']
+  styleUrls: ['./my-gis-component.component.css'],
+  providers: [ MyCourseServiceService ]
 })
 export class MyGisComponentComponent implements OnInit {
+  errorMessage: string;
+  mydata: any; 
+  heroes: Courses[] = [];
+  myname: string[];
+  mode = 'Observable';
+  myObj: any;
+    mgmtTypeList = ['Government', 'Govt Aided',
+            'Govt. Unaided', 'Private'];
+            model= { instname : ""};
+             newReg() { alert('hi'+ this.model.instname) };
+  constructor(private CourseService: MyCourseServiceService) { 
+    this.myObj = [{
+      name: 'Juri',
+      website: 'http://juristr.com',
+      twitter: '@juristr'
+    }];
+       this.mydata = [
+  
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "qui est esse",
+    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+    "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+  }
 
-  constructor() { }
+];
+    
+  }
 
   ngOnInit() {
+    this.getCourse();
   }
+   getCourse() {
+    this.CourseService.getCourses()
+                     .subscribe(
+                       heroes => this.heroes = heroes,
+                       error =>  this.errorMessage = <any>error);             
+  }
+
+  
+  
 
 }
