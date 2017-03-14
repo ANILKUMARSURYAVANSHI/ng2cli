ad3234f2614432e6303572c4cef638e9905c0e76
anil suryavanshi <thakurinfotech@gmail.com>
2017-03-14 22:52:29 +0530

    New api

--- src/app/my-course-service.service.ts
+++ src/app/my-course-service.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response }          from '@angular/http';
+import { Http, Response, Headers, RequestOptions }          from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
@@ -11,16 +11,34 @@ import 'rxjs/add/operator/toPromise';
 
 export class MyCourseServiceService {
   private mytype : string;
-  private heroesUrl = 'https://jsonplaceholder.typicode.com/posts/';  // URL to web API
+  private gisUrl = 'https://jsonplaceholder.typicode.com/posts/';
+  private gisUrlAdd = 'https://jsonplaceholder.typicode.com/posts/'; 
+  private gisUrlUpdate = 'https://jsonplaceholder.typicode.com/posts/';
+  private gisUrlDelete = 'https://jsonplaceholder.typicode.com/posts/'; // URL to web API
   constructor (private http: Http) {}
   getCourses (): Observable<Courses[]> {
-    return this.http.get(this.heroesUrl)
+    return this.http.get(this.gisUrl)
+                    .map(this.extractData)
+                    .catch(this.handleError);
+  }
+  addCourse (name: string): Observable<Courses> {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    return this.http.post(this.gisUrl + this.gisUrlAdd, { name }, options)
+                    .map(this.extractData)
+                    .catch(this.handleError);
+  }
+  updateCourse (name: string): Observable<Courses> {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    return this.http.post(this.gisUrl + this.gisUrlUpdate, { name }, options)
                     .map(this.extractData)
                     .catch(this.handleError);
   }
   private extractData(res: Response) {
     let body = res.json();
-    return body.data || { };
+    //body.data
+    return body || { };
   }
   private handleError (error: Response | any) {
     // In a real world app, we might use a remote logging infrastructure
